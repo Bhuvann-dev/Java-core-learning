58fd1171d404ed20be7e86527cbf6db1f2e14a29
Bhuvan N <bhuvann104@gmail.com>
2026-02-21 16:42:52 +0530

    Static block example

---
+++ oops/Static/src/Practice.java
@@ -0,0 +1,8 @@
+public class Practice {
+
+    static int a = 0;
+    
+     {
+        a++;
+    }
+}
---
+++ oops/Static/src/practiceApp.java
@@ -0,0 +1,16 @@
+public class practiceApp {
+    
+    public static void main(String[] args) {
+        
+        Practice p1 = new Practice();
+       
+        Practice p2 = new Practice();
+       
+        
+
+        System.out.println("no of objects created : " + Practice.a);
+
+
+    }
+    
+}
