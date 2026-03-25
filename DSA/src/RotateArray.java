2ee12c70324094a4643323e322b124bd2ae7db68
Bhuvan N <bhuvann104@gmail.com>
2026-03-25 23:50:52 +0530

    implement left rotation of array by one position

---
+++ DSA/src/RotateArray.java
@@ -0,0 +1,15 @@
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int first = arr[0];
+
+        // Shift elements to the left
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = first;
+
+        System.out.println("Array after left rotation:");
+        for (int i : arr) System.out.print(i + " ");
+    }
+}
