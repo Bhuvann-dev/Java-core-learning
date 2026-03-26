6195b95929bd3f303565df4cc90b3d48c5c2ad99
Bhuvan N <bhuvann104@gmail.com>
2026-03-26 22:46:14 +0530

    feat: solve container with most water using two-pointer approach

---
+++ DSA/src/ContainerWithWater.java
@@ -0,0 +1,17 @@
+public class ContainerWithWater {
+    public static int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+        return maxArea;
+    }
+    public static void main(String[] args) {
+        int[] heights = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println("Max area: " + maxArea(heights));
+    }
+}
