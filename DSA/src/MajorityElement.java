a88d66ac6f2c63e54dc870861604983a109ef81a
Bhuvan N <bhuvann104@gmail.com>
2026-04-02 22:49:50 +0530

    feat: implement Boyer-Moore voting algorithm for majority element

---
+++ DSA/src/MajorityElement.java
@@ -0,0 +1,14 @@
+public class MajorityElement {
+    public static int find(int[] nums) {
+        int count = 0, candidate = 0;
+        for (int num : nums) {
+            if (count == 0) candidate = num;
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println("Majority Element: " + find(nums));
+    }
+}
