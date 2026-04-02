1e6df3301e3027addff8d45049a56143046c4d5f
Bhuvan N <bhuvann104@gmail.com>
2026-04-02 22:51:32 +0530

    feat: add recursive function to print digits of a number in reverse

---
+++ DSA/src/ReverseNumberRec.java
@@ -0,0 +1,14 @@
+public class ReverseNumberRec {
+    public static void reverse(int n) {
+        if (n < 10) {
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n % 10);
+        reverse(n / 10);
+    }
+    public static void main(String[] args) {
+        System.out.print("Reverse of 54321: ");
+        reverse(54321);
+    }
+}
