7ed8948947bad0439e16fe6fd36b261f97bb4266
Bhuvan N <bhuvann104@gmail.com>
2026-04-11 22:10:35 +0530

    feat: implement Sieve of Eratosthenes for efficient prime finding

---
+++ DSA/src/SievePrimes.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class SievePrimes {
+    public static void findPrimes(int n) {
+        boolean[] prime = new boolean[n + 1];
+        Arrays.fill(prime, true);
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p]) {
+                for (int i = p * p; i <= n; i += p) prime[i] = false;
+            }
+        }
+        for (int i = 2; i <= n; i++) if (prime[i]) System.out.print(i + " ");
+    }
+    public static void main(String[] args) {
+        findPrimes(30);
+    }
+}
