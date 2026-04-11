b45bd8c8ffaa8c5f8c570817df228b936a8d7f3b
Bhuvan N <bhuvann104@gmail.com>
2026-04-11 22:09:40 +0530

    feat: implement selection sort algorithm for array ordering

---
+++ DSA/src/SelectionSort.java
@@ -0,0 +1,19 @@
+public class SelectionSort {
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) minIdx = j;
+            }
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] data = {64, 25, 12, 22, 11};
+        sort(data);
+        for (int i : data) System.out.print(i + " ");
+    }
+}
